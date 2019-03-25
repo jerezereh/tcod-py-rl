66ea9c6651c01dc4f0ce89fc9dc6b442b6eda1e2
Max <mavop01@gmail.com>
2019-03-25 15:06:46 -0500

    Finished part 2 of tutorial.

--- engine.py
+++ engine.py
@@ -2,38 +2,51 @@ import tcod as libtcod
 import tcod.event
 import tcod.console
 
-import input_handlers
+from input_handlers import handle_keys
+from entity import Entity
+from map_objects.game_map import GameMap
+from render_functions import clear_all, render_all
 
 
 def main():
     screen_width = 80
     screen_height = 60
-    panel_height = int(screen_height / 2)
+    map_width = 80
+    map_height = 45
+    # panel_height = int(screen_height / 2)
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2) - 5, int(screen_height / 2), '@', libtcod.yellow)
+    entities = [player, npc]
 
     libtcod.console_set_custom_font("img/terminal12x12_gs_ro.png",
                                     libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_CP437)
     libtcod.sys_set_fps(60)
     root = libtcod.console_init_root(screen_width, screen_height, 'PyRL', False, renderer=libtcod.RENDERER_SDL2)
     con = libtcod.console.Console(screen_width, screen_height)
-    panel = libtcod.console.Console(screen_width, panel_height)
+    # panel = libtcod.console.Console(screen_width, panel_height)
 
     # root.print(screen_width // 2 - 7, screen_height // 2 - 12, 'RogueSouls', fg=libtcod.crimson,
     #            bg=libtcod.darkest_han)
     # root.print(screen_width // 2 - 8, screen_height // 2 - 10, 'By Jerezereh')
 
+    game_map = GameMap(map_width, map_height)
+
     while True:
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, root, 0, 0)
+        render_all(con, root, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
+        clear_all(con, entities)
 
         for event in libtcod.event.get():
             if event.type == "QUIT":
                 return True
             if event.type == "KEYDOWN":
-                action = input_handlers.handle_keys(event)
+                action = handle_keys(event)
             else:
                 action = None
 
@@ -44,8 +57,8 @@ def main():
 
                 if move:
                     dx, dy = move
-                    player_x += dx
-                    player_y += dy
+                    if not game_map.is_blocked(player.x + dx, player.y + dy):
+                        player.move(dx, dy)
 
                 if exit:
                     return True
---
+++ render_functions.py
@@ -0,0 +1,33 @@
+import tcod as libtcod
+
+
+def render_all(con, dest, entities, game_map, screen_width, screen_height, colors):
+    # Draw all entities in the list
+    for y in range(game_map.height):
+        for x in range(game_map.width):
+            wall = game_map.tiles[x][y].block_sight
+
+            if wall:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+            else:
+                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+
+    for entity in entities:
+        draw_entity(con, entity)
+
+    con.blit(dest, 0, 0, 0, 0, screen_width, screen_height)
+
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+
+def draw_entity(con, entity):
+    libtcod.console_set_default_foreground(con, entity.color)
+    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+
+
+def clear_entity(con, entity):
+    # erase the character that represents this object
+    libtcod.console_put_char(con, entity.x, entity.y, ' ', libtcod.BKGND_NONE)
