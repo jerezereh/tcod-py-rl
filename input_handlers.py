1431f7942663937622f616a495fd0e5612425014
Max <mavop01@gmail.com>
2019-03-26 09:22:29 -0500

    Finished part 6 of tutorial.

--- input_handlers.py
+++ input_handlers.py
@@ -4,14 +4,22 @@ import tcod.event
 
 def handle_keys(key):
     # Movement keys
-    if key.sym == libtcod.event.K_UP:
+    if key.sym == libtcod.event.K_UP or key.sym == libtcod.event.K_i:
         return {'move': (0, -1)}
-    elif key.sym == libtcod.event.K_DOWN:
+    elif key.sym == libtcod.event.K_DOWN or key.sym == libtcod.event.K_k:
         return {'move': (0, 1)}
-    elif key.sym == libtcod.event.K_LEFT:
+    elif key.sym == libtcod.event.K_LEFT or key.sym == libtcod.event.K_j:
         return {'move': (-1, 0)}
-    elif key.sym == libtcod.event.K_RIGHT:
+    elif key.sym == libtcod.event.K_RIGHT or key.sym == libtcod.event.K_l:
         return {'move': (1, 0)}
+    elif key.sym == libtcod.event.K_u:
+        return {'move': (-1, -1)}
+    elif key.sym == libtcod.event.K_o:
+        return {'move': (1, -1)}
+    elif key.sym == libtcod.event.K_n:
+        return {'move': (-1, 1)}
+    elif key.sym == libtcod.event.K_COMMA:
+        return {'move': (1, 1)}
 
     if key.sym == libtcod.event.K_RETURN and key.mod == libtcod.event.KMOD_ALT:
         # Alt+Enter: toggle full screen
@@ -21,5 +29,5 @@ def handle_keys(key):
         # Exit the game
         return {'exit': True}
 
-    # No key was pressed
+    # No known key was pressed
     return {}
